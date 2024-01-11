e1a2cb174bdba60dace1bfaf2e88b84cdc02fb3e
dipika-7 <akipid77@gmail.com>
2024-01-11 17:35:56 +0545

    Change config

--- frontend/src/services/httpService.ts
+++ frontend/src/services/httpService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_URL } from "../constants/constant";
+import { API_URL } from "../constants/config";
 
 export const http = axios.create({
   baseURL: API_URL,
