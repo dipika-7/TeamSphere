afd509dcaa7a633898c10530daa4d49989946ff7
dipika-7 <akipid77@gmail.com>
2024-01-15 01:03:55 +0545

    Add user api call function

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -1,7 +1,22 @@
 import { http } from "./httpService";
 import { showToastMessage } from "../utils/responseUtil";
 import { AxiosError } from "axios";
-import { USER_ENDPOINTS } from "../constants/endpoint";
+import { USER_ENDPOINTS, USER_TEAM_ENDPOINTS } from "../constants/endpoint";
+
+export async function getUserProfile() {
+  try {
+    const response = await http.get(USER_ENDPOINTS.PROFILE);
+
+    if (response.status === 200) {
+      return response.data.data;
+    }
+  } catch (error) {
+    console.log({ error });
+    if (error instanceof AxiosError) {
+      showToastMessage("error", error.response?.data.message);
+    }
+  }
+}
 
 export async function getUserByUsername(username: string) {
   try {
@@ -13,6 +28,34 @@ export async function getUserByUsername(username: string) {
     }
   } catch (error) {
     console.log({ error });
+    if (error instanceof AxiosError) {
+      showToastMessage("error", error.response?.data.message);
+    }
+  }
+}
+
+export async function getUserList(teamId: string) {
+  try {
+    const response = await http.get(`${USER_ENDPOINTS.USERS_LIST}/${teamId}`);
+    console.log(response.data);
+    if (response.status === 200) {
+      return response.data.data;
+    }
+  } catch (error) {
+    console.log({ error });
+    if (error instanceof AxiosError) {
+      showToastMessage("error", error.response?.data.message);
+    }
+  }
+}
+
+export async function getTeamListByUserId() {
+  try {
+    const response = await http.get(USER_TEAM_ENDPOINTS.USER_TEAM_BY_USER_ID);
+    if (response.status === 200) {
+      return response.data.data;
+    }
+  } catch (error) {
     if (error instanceof AxiosError) {
       showToastMessage("error", error.response?.data.message);
     }
